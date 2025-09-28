d9ca7e2478e6e97ab73880f6e53fe5e19dfc637a
lishaocheng <gexbob@gmail.com>
2025-09-28 10:47:22 +0800

    style: 修改C语言的代码风格

--- drivers/hello_mod/hello.c
+++ drivers/hello_mod/hello.c
@@ -1,18 +1,18 @@
 #include <linux/init.h>
-#include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/module.h>
 
 // 模块加载时调用的函数
 static int __init hello_init(void)
 {
-  printk(KERN_INFO "Hello, World! Module loaded.\n");
-  return 0;  // 返回0表示成功
+	printk(KERN_INFO "Hello, World! Module loaded.\n");
+	return 0; // 返回0表示成功
 }
 
 // 模块卸载时调用的函数
 static void __exit hello_exit(void)
 {
-  printk(KERN_INFO "Goodbye, World! Module unloaded.\n");
+	printk(KERN_INFO "Goodbye, World! Module unloaded.\n");
 }
 
 // 指定模块的初始化和清理函数
